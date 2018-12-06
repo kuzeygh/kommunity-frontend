f9f59d43844c4e9579fe4f2c18cb509b1395787c
Selman K <selmanhalid@gmail.com>
2018-12-06 02:31:58 -0800

    adding heroku postbuild

--- razzle.config.js
+++ razzle.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const glob = require('glob-all');
 const PurgecssPlugin = require('purgecss-webpack-plugin');
-const herokuPlugin = require('./.razzle/heroku-plugin')
 
 const PATHS = {
   src: path.join(__dirname, 'src'),
@@ -9,10 +8,6 @@ const PATHS = {
 
 module.exports = {
   modify: (config, { target, dev }, webpack) => {
-
-    // razzle doesn't work on heroku deploys because of process.env.PORT build issue
-    config = herokuPlugin(config, {target, dev}, webpack);
-
     const appConfig = Object.assign({}, config);
 
     // for prod, removing all unnecessary css with purge css
