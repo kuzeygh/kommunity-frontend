3d1ddd01f87065246aa8bf94f7774940ff2c960d
Mustafa Turhan <odunluzikkim@gmail.com>
2018-12-19 23:08:59 +0300

    cookie functions

--- src/api/local-storage.js
+++ src/api/local-storage.js
@@ -1,7 +1,32 @@
 /* global document */
-export const setCookie = (name, value) => {
+export const setCookie = (name, value, days) => {
   const d = new Date();
-  d.setTime(d.getTime() * 24 * 60 * 60 * 1000);
+  const dayCount = days || 365;
+  d.setTime(d.getTime() + dayCount * 24 * 60 * 60 * 1000);
   const expires = `expires= ${d.toUTCString()}`;
   document.cookie = `${name}= ${value}; ${expires}; path=/`;
+};
+
+export const getCookie = cname => {
+  const name = `${cname}=`;
+  const ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return '';
+};
+
+export const checkCookie = cname => {
+  const checkItem = getCookie(cname);
+
+  if (checkItem !== '') {
+    return true;
+  }
+  return false;
 };
