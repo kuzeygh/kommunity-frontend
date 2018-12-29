3c4065c788d76a2f7a728e8d88396ca4b6a878c0
Yasin Tazeoglu <yasinntazeoglu@gmail.com>
2018-12-29 21:48:00 +0300

    Icon button (#98)
    * added icon to button
    
    * edit button.test.js
    
    * edit tailwind.setup.js
    
    * onClick validation
    
    * deleted left and right border of middle button
    
    * Update button.js
    
    * Update button.js
    
    * edit radio button
    
    * implemented popup login and signup (#155)
    
    * implemented popup login and signup
    
    * id prop for input, createref for instance reference & cleanup
    
    * style adjustments, pass captcharesponse to backend
    
    * lint-fix
    
    * notification component enhanced and form error styles implemented. captcha response is now passed to backend for signup validation. fixed a few minor style issues
    
    * update index test to satisfy expectation due to logo update
    
    * lint error fixed
    
    * edit button.js
    
    * fixed lint and test error
    
    * last upadate
    
    * tailwind-setup.js update
    
    * rm minWidth and minHeight
    
    * update button.js
    
    * update tailwin_output.css

--- src/components/ui/button.js
+++ src/components/ui/button.js
@@ -1,67 +1,153 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cls from 'classnames';
-
-const style = {
-  common: 'button inline-block text-center cursor-pointer border border-transparent border-box',
-  container: {
-    custom: '',
-    danger: 'text-white bg-red',
-    outline: 'text-blueyGrey hover:bg-paleGrey border-lightBlueGrey',
-    plain: 'text-primary hover:text-primaryDark bg-transparent',
-    primary: 'text-white bg-primary',
-    secondary: 'text-white bg-blueyGrey',
-  },
-  groupOrder: {
-    first: 'rounded-l-24',
-    last: 'rounded-r-24',
-    middle: 'rounded-none',
-    none: 'rounded-24',
-  },
-  label: {
-    large: 'px-8 py-3 text-lg',
-    medium: 'px-6 py-2 text-base leading-base',
-    small: 'px-6 py-2 text-sm',
-  },
-};
+import { Icon, Loading } from '.';
 
 class UIButton extends React.Component {
   getClassnames = () => {
-    const { disabled, size, groupOrder, styleType } = this.props;
-
-    return cls(
-      style.common,
-      style.groupOrder[groupOrder],
-      style.label[size],
-      style.container[styleType],
-      {
-        // remove border-left on first item except outline styleType
-        'border-l-0': groupOrder === 'first' && ['outline'].indexOf(styleType) < 0,
-        // remove border-right on last item except outline styleType
-        'border-r-0': groupOrder === 'last' && ['outline'].indexOf(styleType) < 0,
-        // remove border-top-bottom and add bordercolor except outline styleType
-        'border-transparent20 border-t-0 border-b-0':
-          groupOrder !== 'none' && ['outline'].indexOf(styleType) < 0,
-        'cursor-not-allowed': disabled,
-        'hover:opacity-80': !disabled && ['primary', 'secondary'].indexOf(styleType) > -1,
-        'opacity-20': disabled && styleType !== 'plain',
-        'text-lightBlueGrey hover:text-lightBlueGrey': disabled && styleType === 'plain',
+    const {
+      disabled,
+      size,
+      groupOrder,
+      styleType,
+      loading,
+      iconOnly,
+      iconLeft,
+      iconRight,
+    } = this.props;
+    const style = {
+      common: {
+        'active:opacity-100': !disabled,
+        'button text-center border-box focus:outline-none': true,
       },
-    );
+      container: {
+        custom: '',
+        danger: {
+          'focus:border-dark': !loading || !disabled,
+          'text-white bg-red border-transparent': true,
+        },
+        outline: {
+          'hover:bg-lightPaleGrey focus:border-blueyGrey active:shadow-button-blueyGrey active:bg-paleGrey':
+            !loading || !disabled,
+          'text-blueyGrey bg-white border-lightBlueGrey': true,
+        },
+        plain: 'text-primary bg-transparent border-transparent',
+        primary: {
+          'active:bg-primaryDarkx active:shadow-button-dark focus:border-dark':
+            !loading && !disabled,
+          'text-white bg-primary border-transparent': true,
+        },
+        secondary: {
+          'active:bg-battleshipGrey active:shadow-button-dark focus:border-dark':
+            !loading && !disabled,
+          'text-white bg-blueyGrey border-transparent': true,
+        },
+      },
+      groupOrder: {
+        first: 'rounded-l-24',
+        last: 'rounded-r-24',
+        middle: 'rounded-none',
+        none: 'rounded-24',
+      },
+      label: {
+        large: cls({ 'px-5 py-3': iconLeft || iconRight, 'px-7 py-3': !iconLeft && !iconRight }),
+        medium: cls({ 'px-4 py-2': iconLeft || iconRight, 'px-6 py-2': !iconLeft && !iconRight }),
+        small: cls({ 'px-3 py-2': iconLeft || iconRight, 'px-5 py-2': !iconLeft && !iconRight }),
+      },
+      textWrapperCommon: {
+        large: 'text-lg',
+        medium: 'text-base leading-base',
+        small: 'text-sm',
+      },
+    };
+
+    return {
+      buttonWrapperStyle: cls(
+        style.common,
+        style.container[styleType],
+        style.textWrapperCommon[size],
+        {
+          [`${style.label[size]} ${style.groupOrder[groupOrder]}`]: !iconOnly,
+          border: styleType !== 'custom' && styleType !== 'plain',
+          // remove border-left on first item except outline styleType
+          'border-l-0': groupOrder === 'first' && ['outline'].indexOf(styleType) < 0,
+          // remove border-right on last item except outline styleType
+          'border-r-0': groupOrder === 'last' && ['outline'].indexOf(styleType) < 0,
+          // remove border-top-bottom and add bordercolor except outline styleType
+          'border-transparent20 border-t-0 border-b-0 border':
+            groupOrder !== 'none' && ['outline'].indexOf(styleType) < 0,
+          'cursor-not-allowed': disabled || loading,
+          'hover:opacity-90':
+            !disabled && !loading && ['primary', 'secondary'].indexOf(styleType) > -1,
+          'opacity-60': disabled && styleType !== 'plain',
+          'rounded-full px-2 py-2': iconOnly,
+        },
+      ),
+      iconsStyle: {
+        iconSizeWithLabel: {
+          large: 20,
+          medium: 18,
+          small: 16,
+        },
+        onlyIconSize: {
+          large: 26,
+          medium: 22,
+          small: 18,
+        },
+      },
+    };
   };
 
   render() {
-    const { extraClassName, label, onClick, type, disabled } = this.props;
-
+    const {
+      extraClassName,
+      label,
+      onClick,
+      type,
+      disabled,
+      iconLeft,
+      iconRight,
+      loading,
+      size,
+      iconOnly,
+    } = this.props;
+    const { buttonWrapperStyle, iconsStyle } = this.getClassnames();
+    const textButton = iconOnly ? null : (
+      <div className={cls('flex items-center justify-center', { 'opacity-0': loading })}>
+        {iconLeft ? (
+          <Icon name={iconLeft} size={iconsStyle.iconSizeWithLabel[size]} className="mr-1 -ml-1" />
+        ) : null}
+        <p>{label}</p>
+        {iconRight ? (
+          <Icon name={iconRight} size={iconsStyle.iconSizeWithLabel[size]} className="ml-1 -mr-1" />
+        ) : null}
+      </div>
+    );
+    const onlyIconButton = iconOnly ? (
+      <Icon name={iconOnly} size={iconsStyle.onlyIconSize[size]} />
+    ) : null;
     return (
       // eslint-disable-next-line react/button-has-type
       <button
-        onClick={onClick}
-        className={cls(this.getClassnames(), extraClassName)}
+        onClick={() => !disabled && !loading && typeof onClick === 'function' && onClick()}
+        className={cls(buttonWrapperStyle, extraClassName)}
         disabled={disabled}
         type={type || 'submit'}
       >
-        {label}
+        <div className="flex items-center justify-center">
+          {textButton}
+          {onlyIconButton}
+          <div
+            className={cls('absolute', {
+              'flex items-center jutify-center': loading,
+              hidden: !loading,
+            })}
+          >
+            <Loading
+              size={iconOnly ? iconsStyle.onlyIconSize[size] : iconsStyle.iconSizeWithLabel[size]}
+            />
+          </div>
+        </div>
       </button>
     );
   }
@@ -70,17 +156,22 @@ class UIButton extends React.Component {
 UIButton.defaultProps = {
   disabled: false,
   groupOrder: 'none',
+  size: 'medium',
+  styleType: 'primary',
 };
 
 UIButton.propTypes = {
   disabled: PropTypes.bool,
   extraClassName: PropTypes.string,
   groupOrder: PropTypes.oneOf(['first', 'middle', 'last', 'none']),
+  iconLeft: PropTypes.string,
+  iconOnly: PropTypes.string,
+  iconRight: PropTypes.string,
   label: PropTypes.string.isRequired,
+  loading: PropTypes.bool,
   onClick: PropTypes.func,
-  size: PropTypes.oneOf(['small', 'medium', 'large']).isRequired,
-  styleType: PropTypes.oneOf(['primary', 'secondary', 'outline', 'danger', 'plain', 'custom'])
-    .isRequired,
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  styleType: PropTypes.oneOf(['primary', 'secondary', 'outline', 'danger', 'plain', 'custom']),
   type: PropTypes.string,
 };
 
